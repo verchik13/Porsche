a72c3d6ac37f85a216e6b178dbf8a6426f3cfd1f
CyberTekSchool <verachimitova85@gmail.com>
2018-06-21 01:06:13 -0400

    OddNumbers

---
+++ eclipse-workspace/OddNumbers/src/oddnumber/OddNumber.java
@@ -0,0 +1,42 @@
+package oddnumber;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int myNumber = 123;
+//		
+//		if(myNumber % 2 == 0) {
+//			System.out.println("Your number is even");
+//			System.out.println("Second line");
+//			
+//		}else {
+//			System.out.println("Your number is odd");
+//			System.out.println("Second line of else");
+//		}
+//		for(int i=1; i<=100;i++) {
+//			if(i%2==0) {
+//				System.out.println(i +" is even");
+//			}else {
+//			System.out.println(i +" is odd");
+		
+		
+		
+		
+//		for(int num=1; num<=5;num++) {
+//			for(int num2=1; num2<=3;num2++) {
+//				System.out.print("abc,");
+		
+		for(int num=1; num<=5;num++) {
+			System.out.println("in outer loop:"+num);
+			
+			for(int num2=1;num2<=3;num2++) {
+				
+		
+	
+	
+	}
+	}
+	}
+	}
+
